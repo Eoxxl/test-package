5e068e4c025e100c3b3bb025e12fa8e5d7f2b527
a.ustinov <a.ustinov@callforce.pro>
2025-07-14 16:21:39 +0500

    release: version bump

---
+++ CHANGELOG.md
@@ -0,0 +1,11 @@
+# hello-lib
+
+## 0.1.0
+
+### Minor Changes
+
+- add new spectacular greeting with new argument that you can add in hello3 funtion
+
+### Patch Changes
+
+- add new greeting
--- hello.ts
+++ hello.ts
@@ -1,3 +1,11 @@
 export function hello(name: string): string {
     return `Hello ${name}!`;
 }
+
+export function hello2(name: string): string {
+    return `Hello ${name}!`;
+}
+
+export function hello3(name: string, s: string): string {
+    return `Hello ${name}! ${s}`;
+}
